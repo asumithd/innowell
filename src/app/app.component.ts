7813014c601a16fdffabf750951dfa225a61f44c
sumithdeepan <sumith.d@falainacloud.com>
2021-11-11 14:40:37 +0530

    new page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'application';
+  images = [
+    { path: '../assets/img/ibm.png' },
+    { path: '../assets/img/apo.png' },
+    { path: '../assets/img/ast.png' },
+    { path: '../assets/img/kpm.png' },
+    { path: '../assets/img/taj.png' },
+    { path: '../assets/img/xer.png' },
+    { path: '../assets/img/tata.png' }
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+    
+    
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    IvyCarouselModule,
     AppRoutingModule
   ],
   providers: [],
