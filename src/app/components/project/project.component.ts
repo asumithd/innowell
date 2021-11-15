1deb88271998cc740024fef90ebef25d4f8232ce
sumithdeepan <sumith.d@falainacloud.com>
2021-11-15 23:09:22 +0530

    responsive pages

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-  paras:any []=[
-    'LEED Platinum with 82 Points, Sustainability, Energy conservation, Compliance with ACCORD were of particular importance to the project and a priority for the client.','Building massing unit sizes and operating parameters have been selected to exceed compliance criteria and to enhance the operational energy efficiency','Through a combination of energy efficient technologies and innovative solutions significant reductions in energy usage against conventional RMG Unit were achieved. This included a reduction of energy use of 65% and a reduction of water consumption of 50%.'
+  paras: any[] = [
+    'LEED Platinum with 82 Points, Sustainability, Energy conservation, Compliance with ACCORD were of particular importance to the project and a priority for the client.', 'Building massing unit sizes and operating parameters have been selected to exceed compliance criteria and to enhance the operational energy efficiency', 'Through a combination of energy efficient technologies and innovative solutions significant reductions in energy usage against conventional RMG Unit were achieved. This included a reduction of energy use of 65% and a reduction of water consumption of 50%.'
   ];
   images = [
     { path: '../assets/img/ibm.png' },
@@ -17,6 +17,13 @@ export class ProjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // if (Number(document.querySelector('carousel')?.clientWidth.toString()) >= 400) {
+    //   console.log('yes');
+    //   document.getElementsByClassName('threeCarousel carousel')[0].classList.add('width550')
+    //   // document.querySelector('carousel')?.setAttribute('width', '550px')
+    // } else {
+
+    // }
   }
 
 }
