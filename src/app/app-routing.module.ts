01e721eef81154c5504608b0aea7af56cbea169f
sumithdeepan <sumith.d@falainacloud.com>
2021-11-15 10:39:39 +0530

    responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { PeopleComponent } from './components/people/people.component';
+import { ProjectComponent } from './components/project/project.component';
 
 const routes: Routes = [
   { path : 'home', component:HomeComponent },
+  { path : '', component:HomeComponent },
   { path : 'about', component:AboutComponent },
   { path : 'people', component:PeopleComponent },
+  { path : 'project', component:ProjectComponent },
 ];
 
 @NgModule({
--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
-
+  paras:any []=[
+    'LEED Platinum with 82 Points, Sustainability, Energy conservation, Compliance with ACCORD were of particular importance to the project and a priority for the client.','Building massing unit sizes and operating parameters have been selected to exceed compliance criteria and to enhance the operational energy efficiency','Through a combination of energy efficient technologies and innovative solutions significant reductions in energy usage against conventional RMG Unit were achieved. This included a reduction of energy use of 65% and a reduction of water consumption of 50%.'
+  ];
+  images = [
+    { path: '../assets/img/ibm.png' },
+    { path: '../assets/img/apo.png' },
+    { path: '../assets/img/ast.png' }
+  ]
   constructor() { }
 
   ngOnInit(): void {
