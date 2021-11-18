0378d5da46ef1ae3c3f2876db498b8340b944209
sumithdeepan <sumith.d@falainacloud.com>
2021-11-18 20:50:42 +0530

    responsive page

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -10,9 +10,9 @@ export class ProjectComponent implements OnInit {
     'LEED Platinum with 82 Points, Sustainability, Energy conservation, Compliance with ACCORD were of particular importance to the project and a priority for the client.', 'Building massing unit sizes and operating parameters have been selected to exceed compliance criteria and to enhance the operational energy efficiency', 'Through a combination of energy efficient technologies and innovative solutions significant reductions in energy usage against conventional RMG Unit were achieved. This included a reduction of energy use of 65% and a reduction of water consumption of 50%.'
   ];
   images = [
-    { path: '../assets/img/ibm.png' },
-    { path: '../assets/img/apo.png' },
-    { path: '../assets/img/ast.png' }
+    { path: '../assets/img/bg1.jpg' },
+    { path: '../assets/img/bg2.jpg' },
+    { path: '../assets/img/bg3.jpg' }
   ]
   constructor() { }
 
